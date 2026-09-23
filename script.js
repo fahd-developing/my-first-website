1560a8f12e923cdedac7e1763d0a26cc895265cd
fahd-developing <fahdalboraihi@gmail.com>
2026-09-23 17:09:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function changeMessage() {
-    document.getElementById("message").innerHTML =
-        "أنا الآن أتعلم JavaScript من الهاتف!";
+function login() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+
+        document.getElementById("message").innerHTML =
+            "تم تسجيل الدخول بنجاح!";
+
+    } else {
+
+        document.getElementById("message").innerHTML =
+            "اسم المستخدم أو كلمة المرور غير صحيحة.";
+
+    }
 }
